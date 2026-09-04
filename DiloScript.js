9ba36cd09ddeee7c2ef6ae084d0fd93d7db8a0db
akucoba231 <aku.coba231@gmail.com>
2026-09-04 20:04:45 +0700

    Update DiloScript.js

--- DiloScript.js
+++ DiloScript.js
@@ -376,7 +376,7 @@ function pilihMateri(id) {
     const materi = dataMateri.find(m => m.id_materi === id);
     let subMateri = document.getElementById('opsiPermainan');
     subMateri.innerHTML = "";
-    let level = ["mudah","menengah","sulit"];
+    let level = ["Mudah","Sedang","Sulit"];
     materi.permainan.forEach((sub,index) => {
         subMateri.innerHTML += `<button class="btn-opsi" type="button" onclick="pilihSubMateri('${id}', ${index})">${sub.nama_permainan} (${level[index]})</button>`;  
     })
